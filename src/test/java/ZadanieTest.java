f40ace15bfe2c305c45c468c334db3bfb8692299
satek233 <macieksalita6@gmail.com>
2025-12-20 17:42:40 +0100

    nie wiem

--- src/test/java/ZadanieTest.java
+++ src/test/java/ZadanieTest.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ZadanieTest {
     @Test
-    void testdziala12() {
-        assertTrue(true);}
-
+    void dziala() {
+        assertTrue(true);
+    }
 }
