e78ee8c0c6f11a4bedc1c564de7f98588737d309
satek233 <macieksalita6@gmail.com>
2025-12-20 17:13:25 +0100

    dodanie zadania i maven  dziala

--- IO/src/test/java/ZadanieTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class ZadanieTest {
-    @Test
-    void testdziala123() {
-        assertTrue(true);
-    }
-}
