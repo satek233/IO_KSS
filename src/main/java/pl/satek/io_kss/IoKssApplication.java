95e3413c234ecb8e89490ba3e383cc1829f2d8d2
satek233 <2krpqvt7df@privaterelay.appleid.com>
2026-01-26 16:32:14 +0100

    spring test1

---
+++ src/main/java/pl/satek/io_kss/IoKssApplication.java
@@ -0,0 +1,13 @@
+package pl.satek.io_kss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoKssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoKssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/satek/io_kss/IoKssApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.satek.io_kss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoKssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
